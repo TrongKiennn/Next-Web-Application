6e216b7f786e4c64c15179f3c9171eb512da2830
Manh Trong Kien <manhtrongkien1901@gmail.com>
2025-06-19 10:46:50 +0700

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function customersPage() {
+  return (
+    <div>
+        <p>Customers Page</p>
+    </div>
+  )
+}
+
+export default customersPage
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function invoicesPage() {
+  return (
+    <div>
+       <p>Invoices Page</p>
+    </div>
+  )
+}
+
+export default invoicesPage
